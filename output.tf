492a8c37faeaaab02ca6f475cbc2b5e0ba14de97
Vijaykumar-Koduru <koduru.vijaykumar.7@gmail.com>
2025-06-25 02:38:20 +0530

    crearte

--- output.tf
+++ output.tf
@@ -16,17 +16,12 @@ output "vpc_id" {
 
 output "public_subnet_ids" {
     value = aws_subnet.public[*].id
-  
 }
-
 
 output "private_subnet_ids" {
-    value = aws_subnet.private[*].id
-  
+    value = aws_subnet.private[*].id 
 }
-
 
 output "database_subnet_ids" {
     value = aws_subnet.database[*].id
-  
 }
